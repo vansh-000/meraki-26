06880e02ce8176d9f925869da0a0b94313a4bd44
DevBoiAgru <79085233+DevBoiAgru@users.noreply.github.com>
2025-12-03 13:08:07 +0530

    fix: prevent sponsor images small screens bug
    This commit:
    - Uses new seeds for sponsor logos placeholders.
    - Renames background image import for clarity.
    - Ensures device size calculations run on initial load
    - Adjusts character image location

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import bgImage from '../assets/about_image3.png';
+import sponsorsBgImg from '../assets/about_image3.png';
 import charArt from '../assets/sponsors_character_art.png';
 
 
@@ -7,35 +7,35 @@ const sponsorImages = [
     {
         x: 154,
         y: 56,
-        url: `https://picsum.photos/seed/hi/200`,
+        url: `https://picsum.photos/seed/testing/200`,
         size: "200px",
         name: "Director",
         leftAnchored: true,
     }, {
         x: 563,
         y: 174,
-        url: `https://picsum.photos/seed/hii/200`,
+        url: `https://picsum.photos/seed/testing1/200`,
         size: "200px",
         name: "Director",
         leftAnchored: true,
     }, {
         x: 90,
         y: 401,
-        url: `https://picsum.photos/seed/hiii/200`,
+        url: `https://picsum.photos/seed/testing2/200`,
         size: "200px",
         name: "Director",
         leftAnchored: true,
     }, {
         x: 539,
         y: 500,
-        url: `https://picsum.photos/seed/hiiii/200`,
+        url: `https://picsum.photos/seed/testing3/200`,
         size: "200px",
         name: "Director",
         leftAnchored: false,
     }, {
         x: 120,
         y: 401,
-        url: `https://picsum.photos/seed/hiiiii/200`,
+        url: `https://picsum.photos/seed/testing4/200`,
         size: "200px",
         name: "Director",
         leftAnchored: false,
@@ -55,6 +55,9 @@ export default function Sponsors() {
             if (w < 768) setSmallDevice(true);
             else setSmallDevice(false);
         }
+
+        // Do the calculations once on the initial load
+        onResize();
         window.addEventListener("resize", onResize);
         return () => window.removeEventListener("resize", onResize);
     }, []);
@@ -72,7 +75,7 @@ export default function Sponsors() {
                 </h2>
 
                 {/* Minecraft Character - Top Right */}
-                <div className="absolute hidden -top-25 right-10 sm:block sm:w-30 sm:h-40 md:w-60 md:h-80 z-10">
+                <div className="absolute hidden top-0 right-10 sm:block sm:w-30 sm:h-40 md:w-60 md:h-80 z-10">
                     <img
                         src={charArt}
                         alt="Minecraft Creeper"
